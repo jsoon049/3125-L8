6e329af87124c92b6e1b222d8f54c7dc60a265f7
Jeremy <jsoon049@uottawa.ca>
2021-07-18 19:24:23 -0400

    Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css'
 export const Footer = () => {
   return (
     <div className="footer">
-      <p className="footer_text">Website designed by Casper the Friendly Ghost</p>
+      <p className="footer_text footerf">Call us at (905) 456-3000 or</p>
+      <p className="footer_text">Visit us at  6685 Pacific Circle, Mississauga, ON L5T 1V6</p>
     </div>
   );
 };
